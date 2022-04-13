136c1a9d8055a73c8ca2ca65332e51962a67155c
Minh <congminh292k@gmail.com>
2022-04-13 15:45:16 +0700

    update

--- script/monitor.sh
+++ script/monitor.sh
@@ -18,6 +18,35 @@ function disconnect(){
     --output HDMI2 --off \
     --output VIRTUAL1 --off
 }
+
+function both() {
+  /usr/bin/xrandr --output eDP1 --mode 1366x768 --pos 1920x312 --rotate normal \
+    --output DP1 --off \
+    --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
+    --output HDMI2 --off \
+    --output VIRTUAL1 --off
+}
    
-/usr/bin/xrandr | grep "HDMI1 connected" &> /dev/null && connect || disconnect
+function HDMI_connected() {
+  /usr/bin/xrandr | grep "HDMI1 connected" &> /dev/null
+}
+
+function lid_open() {
+  cat /proc/acpi/button/lid/LID/state | grep "open" &> /dev/null
+}
+
+if HDMI_connected && ! lid_open
+then
+  connect
+fi
+
+if HDMI_connected && lid_open
+then
+  both
+fi
+
+if ! HDMI_connected && lid_open
+then
+  disconnect
+fi
 
