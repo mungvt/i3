a028ffdd622bdc1e89532098fab79b0d286597eb
Dawn <congminh292k@gmail.com>
2022-06-21 23:30:37 +0700

    back to arch

--- script/monitor.sh
+++ script/monitor.sh
@@ -4,31 +4,31 @@ export DISPLAY=:0
 export XAUTHORITY=$HOME/.Xauthority
 
 function connect(){
-    /usr/bin/xrandr --output eDP1 --off\
-      --output DP1 --off \
-      --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
-      --output HDMI2 --off \
+    /usr/bin/xrandr --output eDP-1 --off\
+      --output DP-1 --off \
+      --output HDMI-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
+      --output HDMI-2 --off \
       --output VIRTUAL1 --off
 }
   
 function disconnect(){
-  /usr/bin/xrandr --output eDP1 --primary --mode 1366x768 --pos 0x0 --rotate normal \
-    --output DP1 --off \
-    --output HDMI1 --off \
-    --output HDMI2 --off \
+  /usr/bin/xrandr --output eDP-1 --primary --mode 1366x768 --pos 0x0 --rotate normal \
+    --output DP-1 --off \
+    --output HDMI-1 --off \
+    --output HDMI-2 --off \
     --output VIRTUAL1 --off
 }
 
 function both() {
-  /usr/bin/xrandr --output eDP1 --mode 1366x768 --pos 1920x312 --rotate normal \
-    --output DP1 --off \
-    --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
-    --output HDMI2 --off \
+  /usr/bin/xrandr --output eDP-1 --mode 1366x768 --pos 1920x312 --rotate normal \
+    --output DP-1 --off \
+    --output HDMI-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
+    --output HDMI-2 --off \
     --output VIRTUAL1 --off
 }
    
 function HDMI_connected() {
-  /usr/bin/xrandr | grep "HDMI1 connected" &> /dev/null
+  /usr/bin/xrandr | grep "HDMI-1 connected" &> /dev/null
 }
 
 function lid_open() {
