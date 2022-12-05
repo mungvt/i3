0fd1e276506b97a006dd16254a50f9136b384f45
mungvt <mung.vt@citigo.com.vn>
2022-12-05 18:43:59 +0700

    fix: i3 for ubuntu 22.04

--- script/monitor.sh
+++ script/monitor.sh
@@ -20,9 +20,9 @@ function disconnect(){
 }
 
 function both() {
-  xrandr --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal \
+  xrandr --output DP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
     --output DP-1 --off \
-    --output HDMI-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal \
+    --output HDMI-1 --mode 1920x1080 --pos 1920x0 --rotate normal \
     --output HDMI-2 --off \
     --output VIRTUAL1 --off
 }
@@ -32,7 +32,7 @@ function HDMI_connected() {
 }
 
 function lid_open() {
-  xrandr | grep "eDP-1 connected" &> /dev/null
+  xrandr | grep "DP-1 connected" &> /dev/null
 }
 
 if HDMI_connected && ! lid_open
